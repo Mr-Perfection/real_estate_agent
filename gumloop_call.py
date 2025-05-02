fca94ee30d704d97f59f33411391f454840e164b
Romamo <romamo@yandex.ru>
2025-05-02 15:00:56 -0700

    Added gumloop

--- gumloop_call.py
+++ gumloop_call.py
@@ -5,7 +5,7 @@ url = "https://api.gumloop.com/api/v1/start_pipeline"
 payload = {
   "user_id": "GOmsNcGoFfYWehO7H7LvKsWPkIe2",
   "saved_item_id": "f8o3ZHfQWw8WSxbwV3H76S",
-  "pipeline_inputs": [{"input_name":"query","value":"90210"}]
+  "pipeline_inputs": [{"input_name":"text","value":"$1,298,000\nStudio 1 ba1,400 sqft - House for sale"}]
 }
 headers = {
   "Authorization": "Bearer [INSERT GUMLOOP API KEY HERE, GENERATE FROM https://gumloop.com/credentials]",
